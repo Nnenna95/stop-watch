99a047a2cbf9e369ec3f0de6c723cad500787b3b
Nnenna95 <ezeonyidonwannenna@gmail.com>
2022-10-08 19:50:40 +0100

    second commit

--- index.js
+++
@@ -1,7 +0,0 @@
-var seconds= document.getElementById('seconds');
-var seconds= document.getElementById('milliseconds');
-var start= document.getElementById('start');
-var reset= document.getElementById('reset');
-var stop= document.getElementById('stop');
-var sec=0;
-var milisec=0
